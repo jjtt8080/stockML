55546dbc0bfb18711d3fa2f20b31fcd8178cee52
jjtt8080 <janet.tsai@gmail.com>
2019-11-06 18:08:37 -0800

    accidentally add the compiled version in previous update. add the correct one

--- finnhub_api.py
+++ finnhub_api.py
@@ -1,9 +1,11 @@
-import requests as requests
-
-
-from datetime import date as date
-from datetime import time as time
-
+# uncompyle6 version 3.5.0
+# Python bytecode 3.7 (3394)
+# Decompiled from: Python 3.7.3 (default, Apr 24 2019, 15:29:51) [MSC v.1915 64 bit (AMD64)]
+# Embedded file name: C:\Users\Umeda\python\stockML\finnhub_api.py
+# Size of source mod 2**32: 1485 bytes
+import requests
+from datetime import date
+from datetime import time
 DEBUG = 4
 INFO = 2
 NONE = 0
@@ -15,23 +17,23 @@ class Finnapi:
     response = ''
     log_level = NONE
 
-
     def __init__(self, key, loglevel):
         self.api_key = key
         self.log_level = loglevel
 
     def log(self, cur_loglevel, msg):
-        if (cur_loglevel <= self.log_level):
+        if cur_loglevel <= self.log_level:
             print(msg)
 
-    def call_api(self, api_action,  params):
-        self.log(DEBUG, "call_api")
-        self.request_string = 'https://finnhub.io/api/v1/stock/' + api_action  + '?symbol=' + self.curr_symbol + '&'
+    def call_api(self, api_action, params):
+        self.log(DEBUG, 'call_api')
+        self.request_string = 'https://finnhub.io/api/v1/stock/' + api_action + '?symbol=' + self.curr_symbol + '&'
         for k in params.keys():
             self.request_string += k
             self.request_string += '='
             self.request_string += params[k]
             self.request_string += '&'
+
         self.request_string += 'token='
         self.request_string += self.api_key
         self.log(DEBUG, self.request_string)
@@ -42,10 +44,10 @@ class Finnapi:
     def getOptionChain(self):
         self.log(INFO, 'Getting option chain for symbol ' + self.curr_symbol)
         params = {}
-        self.log(DEBUG,  params.keys())
-        res = self.call_api('option-chain',  params)
+        self.log(DEBUG, params.keys())
+        res = self.call_api('option-chain', params)
         return res
 
-
     def getCandleStick(self, symbol):
-        response = call_api('candle', symbol,  {'resolution':'10'})
+        response = call_api('candle', symbol, {'resolution': '10'})
+# okay decompiling finnhub_api.pyc
--- ib_api.py
+++ ib_api.py
@@ -1,8 +1,14 @@
+# uncompyle6 version 3.5.0
+# Python bytecode 3.7 (3394)
+# Decompiled from: Python 3.7.3 (default, Apr 24 2019, 15:29:51) [MSC v.1915 64 bit (AMD64)]
+# Embedded file name: C:\Users\Umeda\python\stockML\ib_api.py
+# Size of source mod 2**32: 3980 bytes
 from ib_insync import *
 from ib_insync.contract import *
 from ib_insync.ticker import Ticker
 from time import sleep, strftime
-
+from ib_insync.objects import BarList, BarDataList, RealTimeBarList, AccountValue, PortfolioItem, Position, Fill, Execution, BracketOrder, TradeLogEntry, OrderState, ExecutionFilter, TagValue, PnL, PnLSingle, ContractDetails, ContractDescription, OptionChain, OptionComputation, NewsTick, NewsBulletin, NewsArticle, NewsProvider, HistoricalNews, ScannerSubscription, ScanDataList, HistogramData, PriceIncrement, DepthMktDataDescription
+import asyncio, logging, util, nest_asyncio
 
 class ibapi:
     m_host = '127.0.0.1'
@@ -11,34 +17,48 @@ class ibapi:
     m_ib = None
     m_symbol = ''
     m_contract = None
+    m_connected = False
+
+    def ibapi(self, host, port, clientid):
+        self.__init__(host, port, clientid)
 
     def __init__(self, host, port, clientid):
         self.m_host = host
         self.m_port = port
         self.m_clientid = clientid
+        self.m_ib = IB()
+        self.connect()
 
     def connect(self):
-        self.m_ib = IB()
-        print("connecting to", self.m_host, self.m_port)
-        if (not self.m_ib.isConnected()):
-            self.m_ib.connect(self.m_host, self.m_port, 999)
+        if not self.m_connected:
+            nest_asyncio.apply()
+            asyncio.get_event_loop().set_debug(True)
+            util.logToConsole(logging.INFO)
+            loop = asyncio.get_event_loop()
+            print('in ib_api.connect')
+            if not self.m_ib.isConnected():
+                self.m_ib.connect(self.m_host, self.m_port, self.m_clientid)
+                m_connected = True
+            else:
+                m_connected = True
 
     def disconnect(self):
         self.m_ib.disconnect()
 
     def setSymbol(self, symbol):
         self.m_symbol = symbol
-        self.m_contract = Stock(self.m_symbol, 'NASDAQ', 'USD', includeExpired=False)
+        self.m_contract = Stock((self.m_symbol), 'NASDAQ', 'USD', includeExpired=False)
 
     def reqHist(self, c_list, duration, stepSize):
         b_list = []
         for c in c_list:
-            bar = self.m_ib.reqHistoricalData(c, '', duration, stepSize, "MIDPOINT", False)
+            bar = self.m_ib.reqHistoricalData(c, '', duration, stepSize, 'MIDPOINT', False)
             b_list.append(bar)
+
         return b_list
 
     def reqTickers(self, cList):
-        priceList = self.m_ib.reqTickers(*cList)
+        priceList = (self.m_ib.reqTickers)(*cList)
         return priceList
 
     def reqMktPrice(self, c, genericTickList):
@@ -48,23 +68,34 @@ class ibapi:
     def qualifyContracts(self, symbolList):
         contractList = []
         for s in symbolList:
-            print("symbol", s)
+            print('symbol', s)
             c = Stock(s, 'SMART', 'USD')
             contractList.append(c)
-        contractList = self.m_ib.qualifyContracts(*contractList)
+
+        contractList = (self.m_ib.qualifyContracts)(*contractList)
         return contractList
 
     def reqOptionDetails(self, c_list):
-        t = self.m_ib.reqMktData(c_list[0], "101", False, False)
+        t = self.m_ib.reqMktData(c_list[0], '101', False, False)
         print('len(ib.pendingTickers()):', len(self.m_ib.pendingTickers()))
         print(t.dict())
         self.m_ib.sleep(5)
-
-        print('OI-1 conId', t.contract.conId, 'localSymbol:', t.contract.localSymbol, ',callOI:', t.callOpenInterest,
-                  ',callVolumn:', t.volume, "last:", t.last, ",ask:", t.ask, ",bid:", t.bid, ",askSize:",t.askSize)
+        print('OI-1 conId', t.contract.conId, 'localSymbol:', t.contract.localSymbol, ',callOI:', t.callOpenInterest, ',callVolumn:', t.volume, 'last:', t.last, ',ask:', t.ask, ',bid:', t.bid, ',askSize:', t.askSize)
 
     def reqOptionChain(self, symbol, exp, strikes, optionType):
         c = Option(symbol, exp, strikes, optionType, 'SMART', 100, 'USD')
         c = self.m_ib.qualifyContracts(c)
         return c
 
+    def reqScannerOption(self, scanCode='HIGH_OPT_VOLUME_PUT_CALL_RATIO'):
+        scanSub = ScannerSubscription()
+        scanSub.instrument = 'STK'
+        scanSub.locationCode = 'STK.US.MAJOR'
+        scanSub.scanCode = scanCode
+        tagvalues = []
+        tagvalues.append(TagValue('usdMarketCapAbove', '10000'))
+        tagvalues.append(TagValue('optVolumeAbove', '1000'))
+        tagvalues.append(TagValue('avgVolumeAbove', '5000000'))
+        c = self.m_ib.reqScannerData(scanSub, None, tagvalues)
+        return c
+# okay decompiling ib_api.pyc
