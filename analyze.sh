815193440f62a8a0008848c3c217df1bd6892446
janet.tsai@gmail.com <janet.tsai@gmail.com>
2020-01-14 12:40:08 -0800

    remove pyc

--- analyze.sh
+++ analyze.sh
@@ -2,7 +2,7 @@
 datestr=$(date '+%Y-%m-%d')
 source /home/jane/anaconda3/bin/activate
 cd /home/jane/python/tradeML
-for i in 2014 2015 
+for i in 2014 2015 2016 2017 2018 2019
 do 
    filename="highvol_$i.pickle"
    echo "getting watch list $filename"
